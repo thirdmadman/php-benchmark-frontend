815af25a2bbc8bd842e7aa9673d0caba5c25e15a
thirdmadman <thirdmadman@gmail.com>
2022-05-31 20:13:48 +0400

    feat: use MainPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { Component } from 'react';
+import MainPage from './components/MainPage';
+import './scss/style.scss';
 
 export default class App extends Component {
   render() {
-    return 'That works!';
+    return <MainPage />;
   }
 }
