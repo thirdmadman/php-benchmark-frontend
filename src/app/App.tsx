64b417a0037350bb521dda8ab542bf0230cf1313
thirdmadman <thirdmadman@gmail.com>
2022-06-22 23:47:02 +0400

    feat: add router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Component } from 'react';
 import MainPage from './components/MainPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './scss/style.scss';
 
 export default class App extends Component {
   render() {
-    return <MainPage />;
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="configs" element={<h1>configs</h1>} />
+          <Route path="admin" element={<h1>admin</h1>} />
+          <Route path="about" element={<h1>about</h1>} />
+          <Route path="*" element={<h1>NOT FOUND</h1>} />
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
