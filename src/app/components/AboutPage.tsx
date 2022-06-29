cda7f386d22ba52be03295ce08d7d20001c71320
thirdmadman <thirdmadman@gmail.com>
2022-06-29 20:07:53 +0400

    feat: add AboutPage

---
+++ src/app/components/AboutPage.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PageFooter from './PageFooter';
+import PageHeader from './PageHeader';
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <div className="container">
+        <PageHeader />
+        <div>About</div>
+        <PageFooter />
+      </div>
+    );
+  }
+}
