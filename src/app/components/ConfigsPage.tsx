704d6bd659a71d8096c5aaf0188a1c2c14eb4e1c
thirdmadman <thirdmadman@gmail.com>
2022-06-29 20:08:22 +0400

    feat: add ConfigsPage

---
+++ src/app/components/ConfigsPage.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PageFooter from './PageFooter';
+import PageHeader from './PageHeader';
+
+export default class ConfigsPage extends Component {
+  render() {
+    return (
+      <div className="container">
+        <PageHeader />
+        <div>Configs</div>
+        <PageFooter />
+      </div>
+    );
+  }
+}
