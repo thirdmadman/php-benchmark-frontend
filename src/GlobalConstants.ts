bba97b45e2e4f20b711ea70e4515d363153090a7
thirdmadman <thirdmadman@gmail.com>
2022-06-29 20:07:18 +0400

    feat: add routes const

--- src/GlobalConstants.ts
+++ src/GlobalConstants.ts
@@ -1,3 +1,11 @@
 export class GlobalConstants {
   public static DEFAULT_API_URL = '/api.php';
+
+  public static ROUTE_MAIN = '/';
+
+  public static ROUTE_ADMIN = '/admin';
+
+  public static ROUTE_CONFIGS = '/configs';
+
+  public static ROUTE_ABOUT = '/about';
 }
