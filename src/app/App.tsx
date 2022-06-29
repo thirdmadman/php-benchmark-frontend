e8ca77eb61c7e8b502838904a3099184b373b54c
thirdmadman <thirdmadman@gmail.com>
2022-06-29 20:09:09 +0400

    feat: use pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,20 @@ import { Component } from 'react';
 import MainPage from './components/MainPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './scss/style.scss';
+import ConfigsPage from './components/ConfigsPage';
+import AboutPage from './components/AboutPage';
+import AdminPage from './components/AdminPage';
+import { GlobalConstants } from '../GlobalConstants';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="configs" element={<h1>configs</h1>} />
-          <Route path="admin" element={<h1>admin</h1>} />
-          <Route path="about" element={<h1>about</h1>} />
+          <Route path={GlobalConstants.ROUTE_MAIN} element={<MainPage />} />
+          <Route path={GlobalConstants.ROUTE_CONFIGS} element={<ConfigsPage />} />
+          <Route path={GlobalConstants.ROUTE_ADMIN} element={<AdminPage />} />
+          <Route path={GlobalConstants.ROUTE_ABOUT} element={<AboutPage />} />
           <Route path="*" element={<h1>NOT FOUND</h1>} />
         </Routes>
       </BrowserRouter>
