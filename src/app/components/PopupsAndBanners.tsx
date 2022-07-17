c4eb74122f000b9a8a9ec56adee22581777108c0
thirdmadman <thirdmadman@gmail.com>
2022-07-17 18:17:04 +0400

    feat: add PopupsAndBanners

---
+++ src/app/components/PopupsAndBanners.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { LocalStorageProvider } from '../services/LocalStorageProvider';
+import AuthPopup from './AuthPopup';
+
+export class PopupsAndBanners extends Component {
+  render() {
+    return !LocalStorageProvider.getData()?.authData ? <AuthPopup /> : '';
+  }
+}
