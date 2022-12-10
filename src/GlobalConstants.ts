e43c7d0a21d402833cc04f8268073485ca4a5654
thirdmadman <thirdmadman@gmail.com>
2022-12-11 00:32:35 +0600

    feat: use dev env

--- src/GlobalConstants.ts
+++ src/GlobalConstants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-  public static DEFAULT_API_URL = process.env.DEFAULT_API_URL || '/api.php';
+  public static DEFAULT_API_URL = process.env.NODE_ENV === 'development' ? process.env.DEFAULT_API_URL : '/api.php';
 
   public static ROUTE_MAIN = '/';
 
